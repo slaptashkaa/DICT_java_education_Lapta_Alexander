d62d3498bfaffb7548ff0c3926f832ef2adcf5b7
slaptashka <o.o.lapta@student.khai.edu>
2021-10-22 02:29:52 +0300

    Stage 5 finished.

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -16,24 +16,45 @@ public class hangman {
 
         String trueword = words.get(new Random().nextInt(words.size()));
 
-        String cap = trueword;
-        StringBuffer sb = new StringBuffer(cap);
+        StringBuffer hideword = new StringBuffer(trueword);
 
         for (int i = 0; i < trueword.length(); i++) {
-            if ( i > 1 ) {
-                sb.setCharAt(i, '-');
-            }
+            hideword.setCharAt(i, '-');
         }
 
-        System.out.println("Guess the word " + sb + ":");
+        System.out.println("Guess the word " + hideword + ":");
 
-        Scanner scan = new Scanner(System.in);
-        String word = scan.nextLine();
+        int attempts = 8;
+        while ( attempts != 0) {
+            System.out.print("Input a letter: > ");
+            Scanner scan = new Scanner(System.in);
+            String letter = scan.nextLine();
+            if ( letter == "" ) {
+                continue;
+            }
 
-        if ( word.equals(trueword) ) {
+            if ( trueword.contains(letter) ) {
+                char[] chtrueword = trueword.toCharArray();
+
+                for (int i=0; i<trueword.length(); i++) {
+                    if( chtrueword[i] == letter.charAt(0) )
+                        hideword.setCharAt(i, letter.charAt(0));
+                }
+
+                System.out.println("\n" + hideword);
+            } else {
+                System.out.println("That letter doesn`t appear in the word\n\n" + hideword);
+            }
+            attempts--;
+        }
+
+        System.out.println("\nThanks for playing!\n" +
+                "We`ll see how well you did in the next stage");
+
+        /*if ( hideword.equals(trueword) ) {
             System.out.println("You survived!");
         } else {
             System.out.println("You lost!");
-        }
+        }*/
     }
 }
