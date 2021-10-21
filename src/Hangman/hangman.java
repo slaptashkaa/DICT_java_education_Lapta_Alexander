5d49826d695702ee447cefeca711dbd75492f578
slaptashka <o.o.lapta@student.khai.edu>
2021-10-21 18:35:33 +0300

    Stage 2. Lost and Survived.

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,9 +1,20 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN\n" +
-                "The game will be available soon.");
+                "Guess the word:");
 
+        Scanner scan = new Scanner(System.in);
+        String trueword = "java";
+        String word = scan.nextLine();
+
+        if ( word.equals(trueword) ) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
