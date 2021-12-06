6e6830892bd2519c30d41972ce270a121bce607e
slaptashka <o.o.lapta@student.khai.edu>
2021-12-06 14:55:01 +0200

    Stage 3

--- src/CoffeMachine/coffemachine.java
+++ src/CoffeMachine/coffemachine.java
@@ -1,13 +1,58 @@
 package CoffeMachine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class coffemachine {
     enum Stage {START, GRIND, BOIL, MIX, POURCOFFE, POURMILK, READY}
 
+    static int water;
+    static int milk;
+    static int beans;
+    static int cups;
+    static int money;
+
     public static void main(String[] args) {
-        checkQuantity();
+        makeCoffe();
     }
+
+    public static void makeCoffe() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Write how many ml of water the coffee machine has:\n> ");
+        water = scan.nextInt();
+        System.out.print("Write how many ml of milk the coffee machine has:\n> ");
+        milk = scan.nextInt();
+        System.out.print("Write how many grams of coffee beans the coffee machine has:\n> ");
+        beans = scan.nextInt();
+        System.out.print("Write how many cups of coffee you will need:\n> ");
+        cups = scan.nextInt();
+        int cWater = water/200;
+        int cMilk = milk/50;
+        int cBeans = beans/15;
+        int[] MinCups = {cWater, cMilk, cBeans};
+        Arrays.sort(MinCups);
+        int minCup = MinCups[0];
+        if(cups == minCup) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            if (cups > minCup) {
+                System.out.println("No, I can make only "+ minCup + " cups of coffee.");
+            } else {
+                if (cups < minCup) {
+                    System.out.println("Yes, I can make that amount of coffee (and even " + (minCup - cups) + " more than that)");
+                }
+            }
+        }
+    }
+
+    /*public static void checkStatistics() {
+        System.out.println("The coffee machine has:\n" +
+                water + " of water\n" +
+                milk + " of milk\n" +
+                beans + " of coffee beans\n" +
+                cups +" of disposable cups\n" +
+                money + " of money");
+    }*/
 
     public static void checkQuantity() {
         Scanner scan = new Scanner(System.in);
